b44d9b3bab079576d46f9d58de694c1479373586
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-05-27 15:29:52 -0400

    First commit

---
+++ print_dist_dicts.py
@@ -0,0 +1,71 @@
+
+
+def print_dicts(address, dl):
+
+    x = {address : {"HUB" : dl[0],
+                    "International Peace Gardens" : dl[1],
+                    "Sugar House Park" : dl[2],
+                    "Taylorsville-Bennion Heritage City Gov Off" : dl[3],
+                    "Salt Lake City Division of Health Services" : dl[4],
+                    "South Salt Lake Public Works" : dl[5],
+                    "Salt Lake City Streets and Sanitation" : dl[6],
+                    "Deker Lake" : dl[7],
+                    "Salt Lake City Ottinger Hall" : dl[8],
+                    "Columbus Library" : dl[9],
+                    "Taylorsville City Hall" : dl[10],
+                    "South Salt Lake Police" : dl[11],
+                    "Council Hall" : dl[12],
+                    "Redwood Park" : dl[13],
+                    "Salt Lake County Mental Health" : dl[14],
+                    "Salt Lake County/United Police Dept" : dl[15],
+                    "West Valley Prosecutor" : dl[16],
+                    "Housing Auth. of Salt Lake County" : dl[17],
+                    "Utah DMV Administrative Office" : dl[18],
+                    "Third District Juvenile Court" : dl[19],
+                    "Cottonwood Regional Softball Complex" : dl[20],
+                    "Holiday City Office" : dl[21],
+                    "Murray City Museum" : dl[22],
+                    "Valley Regional Softball Complex" : dl[23],
+                    "City Center of Rock Springs" : dl[24],
+                    "Rice Terrace Pavilion Park" : dl[25],
+                    "Wheeler Historic Farm" : dl[26]
+                    }}
+
+    print(x)
+
+
+dl = [
+3.6,
+13.0,
+7.4,
+10.1,
+5.5,
+7.2,
+14.2,
+10.7,
+14.1,
+6.0,
+6.8,
+6.4,
+14.1,
+10.5,
+8.8,
+8.4,
+13.6,
+5.2,
+6.9,
+13.1,
+4.1,
+4.7,
+3.1,
+7.8,
+1.3,
+8.3,
+0.0
+
+]
+
+print_dicts("Wheeler Historic Farm", dl)
+
+
+# "Wheeler Historic Farm" : "6351 South 900 East 84121"
