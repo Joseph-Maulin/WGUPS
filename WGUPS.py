ddb239f19cb6c615ccad0d0c65827c589d4ecd55
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-05-28 15:29:23 -0400

    add_package complete

--- WGUPS.py
+++ WGUPS.py
@@ -2,7 +2,7 @@
 # need to pack truck 2 with all truck 2 packages
 # group packages that have to be delivered
 # prioritize delivery_deadlines
-
+from data import packages, Distances
 
 
 class Truck:
@@ -15,16 +15,20 @@ class Truck:
 
 
 class Package:
+    # packageID : {"Address", "City", "Zip", "Delivery_Deadline", "Weight", "Truck", "Available", "Packaged With"}
 
-    def __init__(self, packageID, delivery_address, delivery_deadline, delivery_city, delivery_zip_code, weight, status):
+    def __init__(self, packageID, delivery_address, address_Name, delivery_deadline, delivery_city, delivery_zip_code, weight, status, truck=0, available="8:00", packaged_with=[]):
         self.packageID = packageID
         self.delivery_address = delivery_address
+        self.address_Name = address_Name
         self.delivery_deadline = delivery_deadline
         self.delivery_city = delivery_city
         self.delivery_zip_code = delivery_zip_code
         self.weight = weight
         self.status = status
-
+        self.truck = truck
+        self.available = available
+        self.packaged_with = packaged_with
 
 
 class Delivery_Distribution:
@@ -33,11 +37,11 @@ class Delivery_Distribution:
         self.packages = []
 
 
-    def add_package(self, packageID, delivery_address, delivery_deadline, delivery_city, delivery_zip_code, weight, status):
-        self.packages.append(Package(packageID, delivery_address, delivery_deadline, delivery_city, delivery_zip_code, weight, status))
+    def add_package(self, packageID, delivery_address, address_Name, delivery_deadline, delivery_city, delivery_zip_code, weight, status, truck=0, available="8:00", packaged_with=[]):
+        self.packages.append(Package(packageID, delivery_address, address_Name, delivery_deadline, delivery_city, delivery_zip_code, weight, status, truck=truck, available=available, packaged_with=packaged_with))
 
 
-    def lookup_package(self, packageID=None, delivery_address=None, delivery_deadline=None, delivery_city=None, delivery_zip_code=None, weight=None, status=None):
+    def lookup_package(self, packageID=None, delivery_address=None, address_Name=None, delivery_deadline=None, delivery_city=None, delivery_zip_code=None, weight=None, status=None):
         packages = []
 
         for package in self.packages:
@@ -45,6 +49,8 @@ class Delivery_Distribution:
                 packages.append(package)
                 return packages
             elif package.delivery_address == delivery_address:
+                packages.append(package)
+            elif package.address_Name == address_Name:
                 packages.append(package)
             elif package.delivery_deadline == delivery_deadline:
                 packages.append(package)
@@ -69,7 +75,31 @@ class Delivery_Distribution:
 
 
 
-
-
 if __name__ == "__main__":
-    pass
+    dd = Delivery_Distribution()
+    Distances = Distances()
+    for packageID, package_vars in packages.items():
+        truck = 0
+        available = "8:00"
+        packaged_with = []
+
+        if "Truck" in package_vars.keys():
+            truck = package_vars["Truck"]
+        if "Available" in package_vars.keys():
+            available = package_vars["Available"]
+        if "Packaged With" in package_vars.keys():
+            packaged_with = package_vars["Packaged With"]
+
+        dd.add_package(packageID,
+                       package_vars["Address"],
+                       Distances.address_to_place[package_vars["Address"]],
+                       package_vars["Delivery_Deadline"],
+                       package_vars["City"],
+                       package_vars["Zip"],
+                       package_vars["Weight"],
+                       "Not Delivered",
+                       truck=truck,
+                       available=available,
+                       packaged_with=packaged_with)
+
+    print(len(dd.lookup_package(delivery_deadline="10:30")))
